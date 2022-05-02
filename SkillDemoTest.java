4b7372a29ebb2998a1d7a710802724d7cecaa9ed
summerschulte <sschulte@ucsd.edu>
2022-05-01 23:31:00 -0700

    Update SkillDemoTest.java

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,7 +4,7 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void addition() {
-        assertEquals(2, SkillDemo.add(1,2));
+        assertEquals(2, SkillDemo.add(1,1));
 
     }
 
